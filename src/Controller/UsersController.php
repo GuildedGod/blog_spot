a6b210df4dc8b611e1fa3bef3d143931f396c60b
Carlton.Henry <carlton.alvin.henry@gmail.com>
2019-05-07 06:07:37 -0400

    Demo Defyne

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -58,21 +58,6 @@ class UsersController extends AppController
         $this->set('user', $user);
     }
 
-    public function add()
-    {
-        $user = $this->Users->newEntity();
-        if ($this->request->is('post')) {
-            $user = $this->Users->patchEntity($user, $this->request->getData());
-            if ($this->Users->save($user)) {
-                $this->Flash->success(__('The user has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            }
-            $this->Flash->error(__('The user could not be saved. Please, try again.'));
-        }
-        $this->set(compact('user'));
-    }
-
     public function edit($id = null)
     {
         $user = $this->Users->get($id, [
