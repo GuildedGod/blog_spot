a26e9349dab3c8f09a1f9b3b9ebf828d4e4791e1
Carlton.Henry <carlton.alvin.henry@gmail.com>
2019-05-20 10:14:35 -0400

    DOCKER DEPLOY1

--- config/app.default.php
+++ config/app.default.php
@@ -255,9 +255,9 @@ return [
              * the following line and set the port accordingly
              */
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'my_app',
+            'username' => 'cake',
+            'password' => 'cake',
+            'database' => 'defyne',
             /*
              * You do not need to set this flag to use full utf-8 encoding (internal default since CakePHP 3.6).
              */
@@ -298,9 +298,9 @@ return [
             'persistent' => false,
             'host' => 'localhost',
             //'port' => 'non_standard_port_number',
-            'username' => 'my_app',
-            'password' => 'secret',
-            'database' => 'test_myapp',
+            'username' => 'cake',
+            'password' => 'cake',
+            'database' => 'cake',
             //'encoding' => 'utf8mb4',
             'timezone' => 'UTC',
             'cacheMetadata' => true,
